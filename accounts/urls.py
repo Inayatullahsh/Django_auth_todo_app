7bf1ec8728dd14f3e7bca66d9552e5a4e3a6c723
Inayat-Ullah-Khan <Inayatullahoffical@gmail.com>
2021-05-10 02:33:12 +0430

    fixed accounts app bug(not rendering login page).

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path('register', views.register_view, name="register"),
-    path('login', views.login_view, name="login"),
-    path('logout', views.logout_view, name="logout"),
+    path('register-user', views.register_view, name="register"),
+    path('login-user', views.login_view, name="login"),
+    path('logout-user', views.logout_view, name="logout"),
 ]
