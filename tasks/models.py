fd7013f327c080be6ed09c4309f8fb9d5656eb5f
Inayat-Ullah-Khan <Inayatullahoffical@gmail.com>
2021-09-12 17:07:42 +0430

    Changed field name 'complete' to 'is_completed'.

--- tasks/models.py
+++ tasks/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Task(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     text = models.CharField(max_length=40)
-    complete = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
